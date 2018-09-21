dce1d09786db34d75ce979e6eab02edef059551a
btsyganov <btsyganov@i-novus.ru>
2018-09-21 10:23:21 +0300

    initial

---
+++ src/main/java/com/myproject/videostore/VideostoreApplication.java
@@ -0,0 +1,12 @@
+package com.myproject.videostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideostoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideostoreApplication.class, args);
+	}
+}
